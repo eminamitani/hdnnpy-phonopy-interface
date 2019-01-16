5bc44252fede2545821cbe5a03dd8b2bbfe216e3
Emi Minamitani <eminamitani@cello.t.u-tokyo.ac.jp>
2019-01-16 21:25:28 +0900

    using reshape for more flexible use

--- predict2phonon.py
+++ predict2phonon.py
@@ -43,7 +43,7 @@ for i in disps:
 '''
 Here I parse prediction results from hdnnpy
 default output npz name: prediction_result.npz
-here I assume prediction_result.npz contain 
+here I assume prediction_result.npz contain
 energy and force data for all displacement required by phonopy
 with the same ordering in disp.yaml.
 To do so, you need to care about the script for generation of xyz file to running prediction.
@@ -52,7 +52,7 @@ To do so, you need to care about the script for generation of xyz file to runnin
 force_set=[]
 prdata=np.load('prediction_result.npz')
 keys=prdata.files
-    
+
 for ik in keys:
     if(ik.find('force') >0):
         print('tag to store:'+str(ik))
@@ -71,15 +71,14 @@ with open('FORCE_SETS', "w") as fs:
     fs.write(str(natom)+ "\n")
     fs.write(str(len(disps))+'\n')
     fs.write('\n')
-    
+
     for i in range(len(disp)):
         fs.write(str(atom[i])+'\n')
         fs.write("%20.16f %20.16f %20.16f\n" %
             (tuple(disp[i])))
-        
+
         for f in force_set[0][i]:
-            
+            f = f.reshape(-1, 3)
             for l in f:
                 #print(l)
                 fs.write("%15.10f %15.10f %15.10f\n" % (tuple(l)))
-    
