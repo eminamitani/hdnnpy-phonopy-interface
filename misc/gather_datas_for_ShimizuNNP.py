7fd76755920bf4ad45f6950f1b30a4d7ef3dec9d
Emi Minamitani <eminamitani@ims.ac.jp>
2020-07-09 13:30:41 +0900

    output info file

--- misc/gather_datas_for_ShimizuNNP.py
+++ misc/gather_datas_for_ShimizuNNP.py
@@ -16,6 +16,8 @@ $ gather_data_for_ShimizuNNP [samples] [tag] [dir]
 samples: how many samples taken from each directory
 tag: information of samples
 dir : the directory name to store the all file
+
+example: python gather_datas_for_ShimizuNNP.py 10 bulk_Si test-sample
 """
 
 args = sys.argv
@@ -28,31 +30,49 @@ present = pathlib.Path('./')
 dirs = ([p for p in present.glob('scale*') if p.is_dir()])
 counter = 1
 
-for d in dirs:
-    targets=[]
-    subdirs = ([s for s in d.iterdir() if s.is_dir()])
-    #print(subdirs)
-    targets += random.sample(subdirs, samples)
+#file for make binary data step
+with open("input_tag.dat","w") as tagfile:
 
-    print("total target number {}".format(len(targets)))
-    #counter of the file
+    for d in dirs:
+        targets=[]
+        subdirs = ([s for s in d.iterdir() if s.is_dir()])
+        #print(subdirs)
+        targets += random.sample(subdirs, samples)
+
+        print("total target number {}".format(len(targets)))
+        #counter of the file
 
 
-    for target in targets:
-        output=read(str(target)+'/OUTCAR',format="vasp-out")
-        forces=output.get_forces()
-        positions=output.get_positions()
-        energy=output.get_total_energy()
-        natom=len(forces)
-        shutil.copy(str(target)+'/XDATCAR',dir+'/positions_'+tag+str(natom)+"atoms"+"_"+str(counter)+".dat")
-        with open(dir+'/forces_'+tag+str(natom)+"atoms"+"_"+str(counter)+".dat",'w') as out:
-            out.write("--\n")
-            out.write(" POSITION                                       TOTAL-FORCE (eV/Angst) \n")
-            out.write(" ----------------------------------------------------------------------------------- \n ")
-            for i, p in enumerate(positions):
-                out.write(str(p[0]) + " " + str(p[1]) + " " + str(p[2]) + " " + str(forces[i][0]) + " " + str(
-                    forces[i][1]) + " " + str(forces[i][1])+"\n")
-        with open(dir + '/energies_' + tag + str(natom) + "atoms" + "_" + str(counter) + ".dat", 'w') as ene:
-            ene.write(str(energy)+"\n")
-        counter=counter+1
-        print(counter)
+        for target in targets:
+            output=read(str(target)+'/OUTCAR',format="vasp-out")
+            forces=output.get_forces()
+            positions=output.get_positions()
+            energy=output.get_total_energy()
+            natom=len(forces)
+            tagfile.write(tag+str(natom)+"atoms"+"_"+str(counter)+"\n")
+            shutil.copy(str(target)+'/XDATCAR',dir+'/positions_'+tag+str(natom)+"atoms"+"_"+str(counter)+".dat")
+            with open(dir+'/forces_'+tag+str(natom)+"atoms"+"_"+str(counter)+".dat",'w') as out:
+                out.write("--\n")
+                out.write(" POSITION                                       TOTAL-FORCE (eV/Angst) \n")
+                out.write(" ----------------------------------------------------------------------------------- \n ")
+                for i, p in enumerate(positions):
+                    out.write(str(p[0]) + " " + str(p[1]) + " " + str(p[2]) + " " + str(forces[i][0]) + " " + str(
+                        forces[i][1]) + " " + str(forces[i][1])+"\n")
+            with open(dir + '/energies_' + tag + str(natom) + "atoms" + "_" + str(counter) + ".dat", 'w') as ene:
+                ene.write(str(energy)+"\n")
+
+            #very tentative version
+            with open(dir + '/info_' + tag + str(natom) + "atoms" + "_" + str(counter) + ".dat", 'w') as info:
+                info.write("\n")
+                info.write("Total_number_of_ions "+natom+"\n")
+                info.write("\n")
+                info.write("Ions_per_type 1 \n")
+                info.write("\n")
+                info.write("Si "+str(natom)+"\n")
+                info.write("\n")
+                info.write("MD_steps 1 \n")
+
+                
+
+            counter=counter+1
+            print(counter)
